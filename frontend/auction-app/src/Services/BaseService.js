9f6f6a49aa8e77135d747a61cbb408a1763e6180
harunhasic <harunze@hotmail.com>
2020-10-15 19:02:36 +0200

    frontend deployment

--- frontend/auction-app/src/Services/BaseService.js
+++ frontend/auction-app/src/Services/BaseService.js
@@ -8,19 +8,19 @@ export default class BaseService {
 
     baseApi(params) {
         return axios.create({
-            baseURL: process.env.REACT_APP_FRONTEND_ENDPOINT
+            baseURL: process.env.REACT_APP_BACKEND_ENDPOINT
         })(params);
     }
 
     getApi() {
         return axios.get({
-            baseUrl: process.env.REACT_APP_FRONTEND_ENDPOINT
+            baseUrl: process.env.REACT_APP_BACKEND_ENDPOINT
         });
     }
 
     authorizedApi(params) {
         return axios.create({
-            baseURL: process.env.REACT_APP_FRONTEND_ENDPOINT,
+            baseURL: process.env.REACT_APP_BACKEND_ENDPOINT,
             headers: new authHeader()
         })(params);
     }
--- frontend/auction-app/src/server/subcategory.js
+++ frontend/auction-app/src/server/subcategory.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export async function getRandomSubcategories() {
-    return (await axios.get(process.env.REACT_APP_FRONTEND_ENDPOINT + '/subcategories/random')).data;
+    return (await axios.get(process.env.REACT_APP_BACKEND_ENDPOINT + '/subcategories/random')).data;
 };
 
 export async function getAllSubCategories() {
-    return (await axios.get(process.env.REACT_APP_FRONTEND_ENDPOINT + '/subcategories/all')).data;
+    return (await axios.get(process.env.REACT_APP_BACKEND_ENDPOINT + '/subcategories/all')).data;
 };
