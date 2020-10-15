a28ec808953657de68d1c14c5cd701dd6bde1156
harunhasic <harunze@hotmail.com>
2020-10-15 13:10:43 +0200

    new frontend deploy

--- frontend/auction-app/src/components/MyProfile/Profile.jsx
+++ frontend/auction-app/src/components/MyProfile/Profile.jsx
@@ -3,17 +3,11 @@ import AuthService from '../../services/auth-service';
 
 export default class Profile extends Component {
   constructor(props) {
-    super(props);
-    this.authService = new AuthService();
-    this.state = {
-      currentUser: this.authService.getCurrentUser()
-    };
 
-  
   }
 
   render() {
-    const { currentUser } = this.state;
+
 
     return (
       <div className="container">
