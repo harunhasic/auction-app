56c6b9a4ebf7ce460eb0d83640dde72b04b43e08
harunhasic <harunze@hotmail.com>
2020-11-04 11:25:40 +0100

    Frontend deployment

--- frontend/auction-app/src/Services/BaseService.js
+++ frontend/auction-app/src/Services/BaseService.js
@@ -5,13 +5,13 @@ export default class BaseService {
 
     baseApi(params) {
         return axios.create({
-            baseURL: process.env.REACT_APP_BACKEND_ENDPOINT_DEV
+            baseURL: process.env.REACT_APP_BACKEND_ENDPOINT
         })(params);
     }
 
     authorizedApi(params) {
         return axios.create({
-            baseURL: process.env.REACT_APP_BACKEND_ENDPOINT_DEV,
+            baseURL: process.env.REACT_APP_BACKEND_ENDPOINT,
             headers: new authHeader()
         })(params);
     }
